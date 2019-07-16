2efbc7e2b965a9d985327b9a1cf739f1df5e33dd
AprilYoLies <863821569@qq.com>
2019-07-16 12:32:55 +0800

    完成 restful 发布模式下 SnowflakeIdService 的测试

--- snowflake-server/snowflake-tomcat/src/main/java/top/aprilyolies/snowflake/controller/SegmentController.java
+++ snowflake-server/snowflake-tomcat/src/main/java/top/aprilyolies/snowflake/controller/SegmentController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import top.aprilyolies.snowflake.SnowflakeProperties;
 import top.aprilyolies.snowflake.idservice.IdService;
 import top.aprilyolies.snowflake.idservice.IdServiceFactory;
+import top.aprilyolies.snowflake.idservice.support.MachineIdProviderType;
 
 /**
  * @Author EvaJohnson
@@ -16,12 +17,15 @@ import top.aprilyolies.snowflake.idservice.IdServiceFactory;
 
 @RestController
 public class SegmentController {
+    // 从配置文件中读取的配置信息
     private final SnowflakeProperties snowflakeProperties;
-
+    // SnowflakeIdService 实例
+    private final IdService snowIdService;
+    // SegmentIdService 实例
     private final IdService segIdService;
-
+    // SegmentIdService 服务类工厂
     private IdServiceFactory segIdServiceFactory;
-
+    // SnowflakeIdService 服务类工厂
     private IdServiceFactory snowIdServiceFactory;
 
     public SegmentController(@Qualifier("segment") IdServiceFactory segServiceFactory,
@@ -31,18 +35,42 @@ public class SegmentController {
         this.snowIdServiceFactory = snowServiceFactory;
         this.snowflakeProperties = snowflakeProperties;
         this.segIdService = buildSegmentIdService();
+        this.snowIdService = buildSnowflakeIdService();
     }
 
     @RequestMapping("/snowflake/seg/{business}")
-    public String getId(@PathVariable String business) {
+    public String getSegId(@PathVariable String business) {
         return segIdService.generateId(business);
     }
 
+    @RequestMapping("/snowflake/snow")
+    public String getSnowId() {
+        return snowIdService.generateId();
+    }
+
+    // 根据配置信息构建 SegmentIdService 实例
     private IdService buildSegmentIdService() {
+        segIdServiceFactory.setServiceType("segment");
         segIdServiceFactory.setDbUrl(snowflakeProperties.getDbUrl());
         segIdServiceFactory.setUsername(snowflakeProperties.getUsername());
         segIdServiceFactory.setPassword(snowflakeProperties.getPassword());
-        segIdServiceFactory.setServiceType("segment");
+        try {
+            return (IdService) segIdServiceFactory.getObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // 根据配置信息构建 SnowflakeIdService 实例
+    private IdService buildSnowflakeIdService() {
+        segIdServiceFactory.setServiceType("snowflake");
+        segIdServiceFactory.setDbUrl(snowflakeProperties.getDbUrl());
+        segIdServiceFactory.setMachineIdProvider(MachineIdProviderType.valueOf(snowflakeProperties.getMachineIdProvider()));
+        segIdServiceFactory.setUsername(snowflakeProperties.getUsername());
+        segIdServiceFactory.setPassword(snowflakeProperties.getPassword());
+        segIdServiceFactory.setZkHost(snowflakeProperties.getZkHost());
+        segIdServiceFactory.setIdType(snowflakeProperties.getIdType());
         try {
             return (IdService) segIdServiceFactory.getObject();
         } catch (Exception e) {
