834ec0d91fcd6ba52df93d81336706b698fcee0b
AprilYoLies <863821569@qq.com>
2019-07-17 14:19:30 +0800

    修改 server 实例程序的启动日志

--- snowflake-server/snowflake-netty/src/main/java/top/aprilyolies/snowflake/SnowflakeServer.java
+++ snowflake-server/snowflake-netty/src/main/java/top/aprilyolies/snowflake/SnowflakeServer.java
@@ -5,6 +5,8 @@ import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.codec.http.HttpServerCodec;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @Author EvaJohnson
@@ -12,6 +14,8 @@ import io.netty.handler.codec.http.HttpServerCodec;
  * @Email g863821569@gmail.com
  */
 public class SnowflakeServer {
+    Logger logger = LoggerFactory.getLogger(SnowflakeServer.class);
+
     public static void main(String[] args) {
         new SnowflakeServer().start();
     }
@@ -26,9 +30,9 @@ public class SnowflakeServer {
                     .channel(NioServerSocketChannel.class)
                     .childHandler(new SnowflakeChannelInitializer());
             ChannelFuture future = bootstrap.bind(6707).sync();
-            System.out.println("Snowflake netty server started on port 6707.");
-            System.out.println("Get segment id by http://localhost:6707/snowflake/seg/{business}");
-            System.out.println("Get snowflake id by http://localhost:6707/snowflake/snow");
+            logger.info("Snowflake netty server started on port 6707.");
+            logger.info("Get segment id by http://localhost:6707/snowflake/seg/business-name");
+            logger.info("Get snowflake id by http://localhost:6707/snowflake/snow");
             future.channel().closeFuture().sync();
         } catch (InterruptedException e) {
             e.printStackTrace();
--- snowflake-server/snowflake-tomcat/src/main/java/top/aprilyolies/snowflake/controller/SegmentController.java
+++ snowflake-server/snowflake-tomcat/src/main/java/top/aprilyolies/snowflake/controller/SegmentController.java
@@ -1,8 +1,11 @@
 package top.aprilyolies.snowflake.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import top.aprilyolies.snowflake.SnowflakeApplication;
 import top.aprilyolies.snowflake.idservice.IdService;
 import top.aprilyolies.snowflake.idservice.IdServiceFactory;
 
@@ -14,6 +17,7 @@ import top.aprilyolies.snowflake.idservice.IdServiceFactory;
 
 @RestController
 public class SegmentController {
+    Logger logger = LoggerFactory.getLogger(SegmentController.class);
     // SnowflakeIdService 实例
     private final IdService snowIdService;
     // SegmentIdService 实例
@@ -23,6 +27,9 @@ public class SegmentController {
         ClassLoader classLoader = SegmentController.class.getClassLoader();
         this.segIdService = IdServiceFactory.buildIdService(classLoader, "segment", "snowflake.properties");
         this.snowIdService = IdServiceFactory.buildIdService(classLoader, "snowflake", "snowflake.properties");
+        logger.info("Snowflake netty server started on port 6707.");
+        logger.info("Get segment id by http://localhost:8080/snowflake/seg/business-name");
+        logger.info("Get snowflake id by http://localhost:8080/snowflake/snow");
     }
 
     @RequestMapping("/snowflake/seg/{business}")
